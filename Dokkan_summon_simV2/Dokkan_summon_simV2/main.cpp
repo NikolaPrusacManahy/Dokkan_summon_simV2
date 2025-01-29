988dc29b9a06b58fc2f94e2d08778170ecd3b9ad
(Student  C00309098) Nikola Prusac <C00309098@setu.ie>
2025-01-29 17:12:48 +0000

    Start of project

---
+++ Dokkan_summon_simV2/Dokkan_summon_simV2/main.cpp
@@ -0,0 +1,34 @@
+// Nikola Prusac Manahy
+// C00309098
+// Session time (Approx)
+	// 29.01.25: 1h30
+//----------------------------------------------------------------------------------
+// This program is a re-creation of the Gacha game Dragon Ball Z Dokkan Battle
+// It will simulate a gacha system with probabilities to get certain units
+// Gambling System
+//----------------------------------------------------------------------------------
+// VERSION --> ALPHA
+// ----------------------------------------------------------------------------------
+// Update log --> 
+// ---------------------------------------------------------------------------------
+// LIST OF CORRECTION TO BE DONE!!!!!
+	// 
+	//
+	//
+
+#include<iostream>
+#include<string>
+
+// list of functions
+
+
+
+int main()
+{
+
+
+
+
+	system("pause");
+	return 0;
+}
